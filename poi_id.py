defd140872fcc1d7000d0229b7af11c05befcc96
Bindu <bindu.paul4@gmail.com>
2016-09-05 16:54:34 -0500

    Update poi_id.py

--- poi_id.py
+++ poi_id.py
@@ -3,8 +3,6 @@
 
 import os
 import sys
-#sys.path.append("/Users/bindupaul/Documents/Classes/Udacity/Udacity Data/ud120-projects/tools/")
-#sys.path.append("/Users/bindupaul/Documents/Classes/Udacity/Udacity Data/ud120-projects/final_project/")
 sys.path.append(".../tools/")
 sys.path.append(".../final_project/")
 import pickle
@@ -14,8 +12,6 @@ from sklearn.pipeline import Pipeline
 from sklearn.naive_bayes import GaussianNB
 from sklearn.preprocessing import MinMaxScaler
 from tester import dump_classifier_and_data
-
-#cd Documents/Classes/Udacity/Udacity Data/ud120-projects/final_project
 
 with open("final_project_dataset.pkl", "r") as data_file:
 	data_dict = pickle.load(data_file)
